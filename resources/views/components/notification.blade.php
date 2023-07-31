b87d964dab4760c625d65181e0f033e84ceb0198
Leandro Parra <nixlovemi@gmail.com>
2023-07-31 16:13:19 -0400

    notification adjustment to display HTML

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,4 +1,4 @@
 <div class="alert {{ $notificationClass }} alert-dismissible fade show" role="alert">
     <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">x</button>
-    <strong>{{ $title }} - </strong> {{ $content }}
+    <strong>{{ $title }} - </strong> {!! $content !!}
 </div>
