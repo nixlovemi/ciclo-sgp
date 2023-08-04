11077e8de3e5c6e4307c103f4801682e19a0c978
Leandro Parra <nixlovemi@gmail.com>
2023-08-04 00:16:20 -0400

    chore: new constant

--- app/Helpers/Constants.php
+++ app/Helpers/Constants.php
@@ -4,4 +4,5 @@ namespace App\Helpers;
 
 final class Constants {
     public const USER_DEFAULT_IMAGE_PATH = '/img/no-user.jpg';
+    public const REGEX_PHONE_NUMBER = '/(?=.*[0-9])[- +()0-9]+/';
 }
