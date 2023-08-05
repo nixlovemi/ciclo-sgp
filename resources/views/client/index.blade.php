d01aef011e85cc72cb76d8d623508061fd7cd6a0
Leandro Parra <nixlovemi@gmail.com>
2023-08-05 12:32:30 -0400

    feat: added card to list

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -28,9 +28,13 @@
 
     <div class="row">
         <div class="col-12">
-            <livewire:table
-                :config="App\Tables\ClientsTable::class"
-            />
+            <div class="card mt-2">
+                <div class="card-body px-2 py-0">
+                    <livewire:table
+                        :config="App\Tables\ClientsTable::class"
+                    />
+                </div>
+            </div>
         </div>
     </div>
 @endsection
